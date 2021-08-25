2f93fb2dedecb56ddb0b85a91fefa5be3df8da0e
Musyoki <markmusyoki6@gmail.com>
2021-08-25 18:48:22 +0300

    Add files via upload

---
+++ props-dest.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// CSS
+import './books.css'
+//  setup vars
+const firstbook = {
+author : 'Ann Paul',
+title : 'If Animals Kissed Good Night',
+img : "https://images-na.ssl-images-amazon.com/images/I/51PRQuO-xjL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+}
+const secondbook = {
+author : ' Margaret Wise Brown ',
+title : 'Goodnight Moon',
+img : "https://images-na.ssl-images-amazon.com/images/I/51Ix49rxgtL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+}
+
+
+function BookList() {
+  return (
+      <section className='booklist'>
+         <Book img={firstbook.img} title={firstbook.title} author={firstbook.author}/>
+         <Book title={secondbook.title} img = {secondbook.img} author={secondbook.author}/>
+      </section>
+  ); 
+}
+// 1st way to deconstruct props
+ const Book = ({img, title, author}) =>{  
+    //  second way to deconstruct props
+    // const {img, title, author} = props
+   return ( 
+   <article className="book">
+    <img src={img} alt=''/>   
+    <h2>{author}</h2>
+    <h1>{title}</h1>
+   </article>
+   );
+ }
+ 
+ReactDom.render(<BookList/>, document.getElementById('root'));
